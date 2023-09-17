dbe6943de2bd9c542d0e9d4feb3526113dce15d6
Rupak Lama <rupaklamadev@gmail.com>
2023-09-17 19:17:25 -0400

    created client store for app state

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,13 @@
 "use strict";
 
+import * as store from "./store.js";
+import * as wss from "./wss.js";
+
 // io() - initiate socket.io connection in the client side to the server on the current app port
 // This will help us to Send/Receive events from Client to Server or vice verse
 const socket = io("/");
 
-socket.on("connect", () => {
-  console.log("Connected to the socket.io server from the Client!");
-  // 'socket' is an object that contains information about the current connection
-  console.log(socket.id);
-});
+wss.registerSocketEvents(socket);
 
 // Receiving particular Server Event & args in the callback here
 // note: the argument name can be anything here
---
+++ public/js/wss.js
@@ -0,0 +1,13 @@
+// websocket events
+import * as store from "./store.js";
+import * as ui from "./ui.js";
+
+export const registerSocketEvents = socket => {
+  socket.on("connect", () => {
+    console.log("Connected to the socket.io server from the Client!");
+    // 'socket' is an object that contains information about the current connection
+    console.log(socket.id);
+    store.setSocketId(socket.id);
+    ui.updatePersonalCode(socket.id);
+  });
+};
